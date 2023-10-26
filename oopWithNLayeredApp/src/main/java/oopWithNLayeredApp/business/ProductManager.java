65f5251d281403bcfa4e596cbdc17fd9aca4b979
Rıdvan Ekinci <101838084+rdvneknc@users.noreply.github.com>
2023-10-26 20:59:55 +0300

    adds "oopWithNLayeredApp"

---
+++ oopWithNLayeredApp/src/main/java/oopWithNLayeredApp/business/ProductManager.java
@@ -0,0 +1,34 @@
+package oopWithNLayeredApp.business;
+
+import oopWithNLayeredApp.core.logging.Logger;
+//import oopWithNLayeredApp.dataAccess.HibernateProductDao;
+//import oopWithNLayeredApp.dataAccess.JdbcProductDao;
+import oopWithNLayeredApp.dataAccess.ProductDao;
+import oopWithNLayeredApp.entities.Product;
+
+import java.util.List;
+
+public class ProductManager {
+    private ProductDao productDao;
+    private Logger[] loggers;
+
+    public ProductManager(ProductDao productDao, Logger[] loggers) {
+        this.productDao = productDao;
+        this.loggers = loggers;
+    }
+
+    public void add(Product product) throws Exception {
+
+        if(product.getUnitPrice() < 10){
+            throw new Exception("Ürün Fiyatı 10'dan düşük olamaz");
+        }
+
+        productDao.add(product);
+
+        for (Logger logger: loggers) {
+            logger.log(product.getName());
+        }
+
+
+    }
+}
