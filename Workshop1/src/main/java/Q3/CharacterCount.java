b7827e50f19cdf8c809835c4acb484148ab46212
Rıdvan Ekinci <101838084+rdvneknc@users.noreply.github.com>
2023-10-30 16:26:01 +0300

    adds Q6

--- Workshop1/src/main/java/Q3/CharacterCount.java
+++ Workshop1/src/main/java/Q3/CharacterCount.java
@@ -4,18 +4,20 @@ public class CharacterCount {
 
     public static void main(String[] args) {
 
-        String string = "Oyun bilgisayarı";
+        String string = "Oyun Atı";
         int count = 0;
 
 
+//  3-Write a Java program gives character counts in a string Ex: Tobeto -> t:2, o:2, b:1, e:1
 
-
-        for(int i = 0; i < string.length(); i++) {
-            if(string.charAt(i) != ' ')               //tobeto string.charAt()
+        for(int i = 0; i < string.length(); i++) { //
+            if(string.charAt(i) != ' ')
+            {//tobeto string.charAt()
                 count++;
         }
 
         System.out.println("Total number of characters in this string is : " + count);
+    }
     }
 
 }
---
+++ Workshop1/src/main/java/Q6/AmicablePair.java
@@ -0,0 +1,44 @@
+package Q6;
+
+import java.util.Scanner;
+
+public class AmicablePair {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        int input1 = scan.nextInt();
+
+        System.out.println("Enter another number");
+        int input2 = scan.nextInt();
+
+        int num1 = input1;
+        int num2 = input2;
+
+        int sum1 = 0;
+        int sum2 = 0;
+
+
+        for (int i = 1; i < num1; i++) {
+            if (num1 % i == 0) {
+                sum1 += i;
+            }
+        }
+
+        for (int i = 1; i < num2; i++) {
+            if (num2 % i == 0) {
+                sum2 += i;
+            }
+        }
+
+        if (num1 == sum2 && num2 == sum1) {
+            System.out.println("These two numbers are Amicable");
+        } else {
+            System.out.println("These two numbers not Amicable");
+        }
+    }
+
+
+}
