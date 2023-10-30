a8d1366fa7a4459d783f6684d94e30fff0cb9d13
Rıdvan Ekinci <101838084+rdvneknc@users.noreply.github.com>
2023-10-30 14:19:35 +0300

    adds Q4

--- Workshop1/src/main/java/Q2/Fibonacci.java
+++ Workshop1/src/main/java/Q2/Fibonacci.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 
 public class Fibonacci {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter a number");
 
         double input = scan.nextInt();
+
 
         double number1 = 0;
         double number2 = 1;
@@ -16,12 +18,12 @@ public class Fibonacci {
         System.out.println("Fibonacci series of " + input );
 
         for (int i = 1; i <= input; i++) {
-            System.out.print(number1 + " , ");
+            System.out.print(number1 + " , ");  // 0
 
 
-            sum = number1 + number2;
-            number1 = number2;
-            number2 = sum;
+            sum = number1 + number2;  // 0 + 1   1
+            number1 = number2;       //0  - 1
+            number2 = sum;       // 1 - 2
         }
 
         scan.close();
--- Workshop1/src/main/java/Q3/CharacterCount.java
+++ Workshop1/src/main/java/Q3/CharacterCount.java
@@ -3,12 +3,15 @@ package Q3;
 public class CharacterCount {
 
     public static void main(String[] args) {
-        String string = "Tobeto";
+
+        String string = "Oyun bilgisayarı";
         int count = 0;
 
 
+
+
         for(int i = 0; i < string.length(); i++) {
-            if(string.charAt(i) != ' ')
+            if(string.charAt(i) != ' ')               //tobeto string.charAt()
                 count++;
         }
 
@@ -16,3 +19,4 @@ public class CharacterCount {
     }
 
 }
+
